a40b8aebe52f027e9ed4b3e42a914235cf329d45
3nsar <84131267+3nsar@users.noreply.github.com>
2024-12-06 23:22:40 +0100

    commit

--- tailwind-demo/src/App.tsx
+++ tailwind-demo/src/App.tsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Hero from './components/Hero'
 
 function App() {
 
 
   return (
-    <div className='bg-slate-900 text-emerald-100'>
-      <h1>hello</h1>
+    <div className='h-full w-full'>
+      <div className="flex flex-col gap-20">
+        <Hero/>
+      </div>
+      
     </div>
   )
 }
---
+++ tailwind-demo/src/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='bg-black h-full w-full text-cyan-200 flex flex-col'>
+        <h1>Hey</h1>
+        <p>This is the hero section</p>
+    </div>
+  )
+}
+
+export default Hero
