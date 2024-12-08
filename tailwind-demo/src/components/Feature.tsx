c5da501457ce7b9a8a1b96a981400bc3bc7d7f00
3nsar <84131267+3nsar@users.noreply.github.com>
2024-12-08 15:32:49 +0100

    commit

--- tailwind-demo/src/components/Feature.tsx
+++ tailwind-demo/src/components/Feature.tsx
@@ -10,13 +10,15 @@ const Feature = () => {
         </div>
         <div className="flex flex-wrap mt-10 lg:mt-20">
             {features.map((feature,index)=>(
-                <div key={index} className='w-full sm:1/2 lg:1/3'>
-                    <div className="flex mx-6 h-10 w-10 p-2 bg-neutral-900 text-orange-500 justify-center items-center roundded-full">
-                        {feature.icon}
-                    </div>
-                    <div>
-                        <h5 className='mt-1 mb-6 text-xl'>{feature.text}</h5>
-                        <p>{feature.description}</p>
+                <div key={index} className='w-full sm:w-1/2 lg:w-1/3'>
+                    <div className="flex">
+                        <div className="flex mx-6 h-10 w-10 p-2 bg-slate-800 text-teal-500 justify-center items-center rounded-full">
+                            {feature.icon}
+                        </div>
+                        <div>
+                            <h5 className='mt-1 mb-6 text-lg'>{feature.text}</h5>
+                            <p className='text-md p-2 mb-20 text-neutral-500'>{feature.description}</p>
+                        </div>
                     </div>
                 </div>
             ))}
